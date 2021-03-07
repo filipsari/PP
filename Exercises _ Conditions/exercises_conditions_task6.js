f51d6b24b7532abcb08e3d38e43e767fdc8dc099
Filip Sari <filipsari@gmail.com>
2021-03-07 19:56:14 +0100

    Added new exercise in JS BIT school

---
+++ Exercises _ Conditions/exercises_conditions_task6.js
@@ -0,0 +1,24 @@
+
+
+/*Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double difference between that number and 13.
+	Sample Input: 11					Sample Input: 32
+	Output : 2						Output : 38*/
+
+    var a = 11;
+
+    if (a>13) {
+        console.log(2*(a-13));
+    } else {
+       console.log(13 - a); 
+    }
+    
+
+
+
+
+
+
+
+
+
+
