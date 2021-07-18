22a07d33714cbee2337aca09e79bd668e09680e5
Filip Sari <filipsari@gmail.com>
2021-07-18 13:42:13 +0200

    practise ES6

--- ES6/Pracstice ES6/practisingES6.js
+++ ES6/Pracstice ES6/practisingES6.js
@@ -50,3 +50,92 @@ const sandwich = {
 }
 const { bread, meat } = sandwich
 console.log(bread, meat) 
+
+/* DESTRUCTURING ASSIGNMENT */
+
+class Person {
+  constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+  }
+}
+
+let person1 = new Person("Pera", "Peric", 22);
+
+
+function printFullName(person) {
+  /* let firstName = person.firstName
+  let lastName = person.lastName */   //instead of this, in ES6 we can use this > let { firstName, lastName } = person;
+  let { firstName, lastName } = person;
+  console.log(`${firstName} ${lastName}`)
+}
+
+printFullName(person1);     //Pera Peric
+
+
+
+
+class Person {
+  constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.address = {
+          street: "Topolska",
+          number: 18,
+          city: "Na vratima pise"
+      }
+  }
+}
+
+let person1 = new Person("Pera", "Peric", 22);
+
+function printAddress(person) {
+  let { street, number, city } = person.address;
+  console.log(`${street}, ${number}, ${city}`)
+}
+
+function printFullName({ firstName, lastName }) {
+  console.log(`${firstName} ${lastName}`)
+}
+
+printFullName(person1)
+printAddress(person1)
+
+
+let dogs = ["Snaucer", "Samojed", "Haski"];
+
+let [dog1, dog2] = dogs
+console.log(dog1);  //Snaucer
+console.log(dog2);  //Samojed       //if we want only Haski we will write let [, , dog3] = dogs
+
+
+//The destructuring assignment allows you to locally scope fields within an object and to declare which values will be used. Consider this sandwich object. 
+
+
+let clickHandler = function (e) {
+    if (e.which === 13) {
+        console.log("submit");
+    }
+}
+
+//after DESTRUCTURING IS
+
+let clickHandler = function ({ which }) {
+    if (which === 13) {
+        console.log("submit");
+    }
+}
+
+
+//OR
+
+
+let clickHandler = function ({ which: nameThatSuitsUs }) {
+    if (nameThatSuitsUs === 13) {
+        console.log("submit");
+    }
+}
+
+
