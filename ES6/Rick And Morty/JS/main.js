c9e03539b6e39b146611982a84614b2aca21b651
Filip Sari <filipsari@gmail.com>
2021-08-04 20:45:42 +0200

    Working in process Rick and Morty

--- ES6/Rick And Morty/JS/main.js
+++ ES6/Rick And Morty/JS/main.js
@@ -1,7 +1,7 @@
 // import {myFunction, customFetch} from './methods.js';
 
 let main = document.querySelector("main");
-let mainDiv = document.querySelector(".container")
+let mainDiv = document.querySelector(".container");
 
 // customFetch("https://rickandmortyapi.com/api/character").then(obj_js => {
 //   const{results} = obj_js;
@@ -9,14 +9,14 @@ let mainDiv = document.querySelector(".container")
 
 // console.log({fetchAllCharacters}); // isto sto i: console.log('fetchAllCharacters: ', fetchAllCharacters)
 
+fetch("https://rickandmortyapi.com/api/character")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (obj_js) {
+    const { results } = obj_js;
 
-fetch("https://rickandmortyapi.com/api/character").then(function(response){
-  return response.json();
-}).then(function(obj_js){
-
- const{results} = obj_js;
-
- results.forEach(function(element){
+    results.forEach(function (element) {
       let div = document.createElement("div");
       let img = document.createElement("img");
       let name = document.createElement("h4");
@@ -24,37 +24,29 @@ fetch("https://rickandmortyapi.com/api/character").then(function(response){
       img.setAttribute("src", element.image);
       img.classList.add("image");
       div.classList.add("single_div");
-            div.setAttribute("data-key", element.id);
-      name.classList.add("characterName")
+      div.setAttribute("data-key", element.id);
+      name.classList.add("characterName");
       name.textContent = element.name;
 
       div.append(img);
       div.append(name);
       mainDiv.append(div);
       main.append(mainDiv);
+    });
+
+    // myFunction();
+
+    let divs = document.querySelectorAll(".single_div");
+
+    divs.forEach(function (for_every_div) {
+      for_every_div.addEventListener("click", function () {
+        let key = for_every_div.getAttribute("data-key");
+        window.localStorage.setItem("id", key);
+        document.location = "second.html";
+      });
+    });
   })
-
-
-  // myFunction();
-
-  let divs = document.querySelectorAll(".single_div");
-
-  divs.forEach(function(for_every_div){
-
-    for_every_div.addEventListener("click", function(){
-    let key = for_every_div.getAttribute("data-key");
-    window.localStorage.setItem("id", key) 
-    document.location = "second.html";
-
-    })
-  })
-
-
-
-}).catch(function(error){
-  console.error("some error happend")
-  alert("Error occured, please reload the page")
-})
-
-
-
+  .catch(function (error) {
+    console.error("some error happend");
+    alert("Error occured, please reload the page");
+  });
